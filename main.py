42f0f5f6f02f00c16c725f10e629eb662026bea2
alex <zeroalexchen@outlook.com>
2021-07-16 09:23:33 +0800

    添加温度曲线，增加一种特殊情况下的cap_dev_percentage计算方法

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import glob
 import re
 import time
@@ -23,28 +24,27 @@ custom_type = False
 '''
 custom_name = ['TimeName', 'VoltageName', 'CurrentName', 'RSOCName', 'RCName', 'FCCName']
 
+# 时间显示线程使能
 g_time_flag = 0
+
+# 写入参数
 g_author = ''
 g_chr_voltage = 0
 g_term_voltage = 0
 g_fw_version = ''
+g_project_name = ''
 
 
 # 获取文件夹下所有log后缀的文件名
 def get_file_name():
     filename = []
-
     for i in glob.glob(r'./*.log'):
         filename.append(i)
-
     for i in range(len(filename)):
         print(" %d : %s " % (i + 1, filename[i][2:]))
-
     file_num = input('\n输入文件编号：')
-
     while not file_num.isdigit() or (int(file_num) - 1) >= len(filename):
         file_num = input('输入错误，请重新输入：')
-
     return filename[int(file_num) - 1]
 
 
@@ -64,9 +64,12 @@ def time_count():
 
 
 class BuildExcel:
-    def __init__(self, log_name, ex_name):
+    def __init__(self, log_name):
         self.file_path = log_name
-        self.excel_path = './result/' + ex_name + '.xlsx'
+        # 根据project name生成Excel文件名
+        self.excel_path = ('./result/{0}-Battery-Cycle-Test-Curve-Rev{1}-{2:02}{3:02}{4}.xlsx'
+                           .format(g_project_name, g_fw_version, datetime.datetime.now().month,
+                                   datetime.datetime.now().day, datetime.datetime.now().year))
         self.log_name = None
 
         self.cycle_count = 0
@@ -78,15 +81,15 @@ class BuildExcel:
     # 获取log数据中对应的模块名
     @staticmethod
     def get_module_name(line):
-        if ('ElapsedTime' in line and 'Voltage' in line and 'Current' in line
-                and 'RSOC' in line and 'RemCap' in line and 'FullChgCap' in line):
+        if ('ElapsedTime' in line and 'Voltage' in line and 'Current' in line and 'RSOC' in line
+                and 'RemCap' in line and 'FullChgCap' in line and 'Temperature' in line):
 
-            return ['ElapsedTime', 'Voltage', 'Current', 'RSOC', 'RemCap', 'FullChgCap']
+            return ['ElapsedTime', 'Voltage', 'Current', 'RSOC', 'RemCap', 'FullChgCap', 'Temperature']
 
-        elif ('~Elapsed(s)' in line and 'Voltage' in line and 'AvgCurrent' in line
-              and 'StateofChg' in line and 'RemCap' in line and 'FullChgCap' in line):
+        elif ('~Elapsed(s)' in line and 'Voltage' in line and 'AvgCurrent' in line and 'StateofChg' in line
+              and 'RemCap' in line and 'FullChgCap' in line and 'Temperature' in line):
 
-            return ['~Elapsed(s)', 'Voltage', 'AvgCurrent', 'StateofChg', 'RemCap', 'FullChgCap']
+            return ['~Elapsed(s)', 'Voltage', 'AvgCurrent', 'StateofChg', 'RemCap', 'FullChgCap', 'Temperature']
 
         return False
 
@@ -100,6 +103,7 @@ class BuildExcel:
         except:
             pass
 
+        # 当获取到begin_value后，定义这一行为开始行
         for i in range(len(line)):
             if begin_value in line[i]:
                 begin_num = i
@@ -107,28 +111,33 @@ class BuildExcel:
             else:
                 continue
 
+        # 获取分隔符
         if ',' in line[begin_num]:
             delimiter = ','
         elif '\t' in line[begin_num]:
             delimiter = '\t'
 
-        i = begin_num
-
+        # 获取log数据中个模块的名称
         self.log_name = self.get_module_name(line[begin_num])
 
+        # 若没有匹配数据，则检测有没有设置自定义模块
         if not self.log_name:
             if custom_type:
                 self.log_name = custom_name
             else:
                 return False
 
+        i = begin_num
+        # 根据分隔符将log数据分隔
         while i < len(line):
             line[i] = line[i].split(delimiter)
             i += 1
 
+        # 生成新的二维数组
         new_line = line[begin_num:]
 
         for i in range(len(new_line)):
+            # 定义首行数据，获取各数据模块的位置
             if i == 0:
                 time_num = new_line[i].index(self.log_name[0])
                 voltage_num = new_line[i].index(self.log_name[1])
@@ -136,7 +145,8 @@ class BuildExcel:
                 rsoc_num = new_line[i].index(self.log_name[3])
                 rc_num = new_line[i].index(self.log_name[4])
                 fcc_num = new_line[i].index(self.log_name[5])
-                new_line[i].extend([' ', 'Time', 'Voltage', 'Current', 'RSOC', 'RC', 'FCC',
+                temp_num = new_line[i].index(self.log_name[6])
+                new_line[i].extend([' ', 'Time', 'Voltage', 'Current', 'RSOC', 'RC', 'FCC', 'Temperature',
                                     ' ', 'Accumulated', 'Deviation', 'Fuel Gauge Deviation', 'Fuel Gauge Accuracy'])
 
             else:
@@ -153,14 +163,19 @@ class BuildExcel:
                     new_line[i][rc_num] = 0
                 if not new_line[i][fcc_num]:
                     new_line[i][fcc_num] = 0
+                if not new_line[i][temp_num]:
+                    new_line[i][temp_num] = 0
 
+                ''' 将添加特定数据 '''
+                # 部分芯片通讯出现error时，新生成的数据的位置会被打乱，程序可以自动修复
                 if re.search('error', new_line[i][-1], re.IGNORECASE) and '~Elapsed(s)' in new_line[0]:
                     new_line[i].extend([round(float(new_line[i][time_num]) / 3600, 6),
                                         int(new_line[i][voltage_num]),
                                         abs(int(new_line[i][current_num])),
                                         int(new_line[i][rsoc_num]),
                                         int(new_line[i][rc_num]),
-                                        int(new_line[i][fcc_num])])
+                                        int(new_line[i][fcc_num]),
+                                        float(new_line[i][temp_num])])
 
                 else:
                     new_line[i].extend([' ',
@@ -169,27 +184,32 @@ class BuildExcel:
                                         abs(int(new_line[i][current_num])),
                                         int(new_line[i][rsoc_num]),
                                         int(new_line[i][rc_num]),
-                                        int(new_line[i][fcc_num])])
-
+                                        int(new_line[i][fcc_num]),
+                                        float(new_line[i][temp_num])])
+        # 计算容量
         self.cap_accumulated(new_line)
 
+        # 将new_line的数据生成excel
         df = pd.DataFrame(new_line)
         df.to_excel(self.excel_path, header=None, index=False)
 
         return True
 
     def cap_accumulated(self, line):
-        fcc_num = int(len(line[1])) - 1
+        # 获取新添加的数据的位置
+        fcc_num = int(len(line[1])) - 2
         rc_num = fcc_num - 1
         rsoc_num = rc_num - 1
         current_num = rsoc_num - 1
         voltage_num = current_num - 1
         time_num = voltage_num - 1
 
+        # 充放电标志位，chg_flag 和 disg_flag同时等于1时，自动计算容量程序才会开启
+        # 即在检查到充电后，下一个放电操作才会计算容量
         chg_flag = 0
         disg_flag = 0
-        i = 1
 
+        i = 1
         while i < len(line):
             global g_term_voltage
             zero_num = 0
@@ -228,6 +248,7 @@ class BuildExcel:
 
                     line[begin_num - 1].extend([' ', 0])
                     for n in range(begin_num, end_num):
+                        # 容量计算公式
                         temp_cap = ((line[n][time_num] - line[n - 1][time_num]) *
                                     (line[n][current_num] + line[n - 1][current_num]) / 2 + line[n - 1][-1])
 
@@ -235,6 +256,13 @@ class BuildExcel:
 
                     cap_dev = line[zero_num][-1] - line[term_num][-1]
                     cap_dev_percentage = cap_dev / line[term_num][-1]
+
+                    ''' 特殊情况1 '''
+                    # 当RSOC瞬间跳为0时，检测上一时刻是否为1，若不是，cap_dev_percentage就为两个时刻的rsoc的差值
+                    if cap_dev_percentage == 0:
+                        if line[zero_num-1][rsoc_num] > 1:
+                            cap_dev_percentage = (line[zero_num][rsoc_num]-line[zero_num-1][rsoc_num])/100
+
                     cap_percentage = line[term_num][-1] / line[begin_num][fcc_num]
                     if cap_percentage > 1:
                         cap_percentage = 1 / cap_percentage
@@ -256,50 +284,59 @@ class BuildExcel:
         sheet = file.active
         sheet.freeze_panes = 'A2'
 
+        # 建立图表工作表
         chart_sheet = file.create_chartsheet('Chart1')
 
-        chart = ScatterChart()
+        chart1 = ScatterChart()
+        chart2 = ScatterChart()
 
-        chart_rsoc = ScatterChart()
-
+        # cycle test结果输出
         result_title = ''
         for i in self.cycle_result:
             result_title += '{0} : {1}   '.format(i, self.cycle_result[i])
 
-        chart.title = ('Project Name Cycle-Test-Curve\n'
-                       '\t\tF/W: {0},   Charge : {1}V/{2}A,   Discharge : {3}A\n'
-                       '{4}\n'
-                       '\t\t\t\t\tTested by:{5}'.format(g_fw_version, g_chr_voltage, self.chr_current,
-                                                        self.disg_current, result_title, g_author))
+        # 图表标题
+        chart1.title = ('{0} Battery Pack Cycle-Test-Curve\n'
+                        '\t\tF/W: {1},   Charge : {2}V/{3}A,   Discharge : {4}A\n'
+                        '{5}\n'
+                        '\t\t\t\t\tTested by:{6}'
+                        .format(g_project_name, g_fw_version, g_chr_voltage,
+                                self.chr_current, self.disg_current, result_title, g_author))
 
-        xvalue = Reference(sheet, min_row=2, min_col=sheet.max_column - 10,
-                           max_row=sheet.max_row, max_col=sheet.max_column - 10)
+        # x轴为时间
+        xvalue = Reference(sheet, min_row=2, min_col=sheet.max_column - 11,
+                           max_row=sheet.max_row, max_col=sheet.max_column - 11)
 
-        for i in range(sheet.max_column - 9, sheet.max_column - 4):
-            yvalue = Reference(sheet, min_row=1, min_col=i,
-                               max_row=sheet.max_row, max_col=i)
+        # 根据不同的数据建立Excel散点图系列
+        for i in range(sheet.max_column - 10, sheet.max_column - 4):
+            yvalue = Reference(sheet, min_row=1, min_col=i, max_row=sheet.max_row, max_col=i)
 
             series = Series(yvalue, xvalue, title_from_data=True)
-            if i == sheet.max_column - 7:
-                chart_rsoc.append(value=series)
+
+            # RSOC和温度分配到另一个y轴
+            if i == sheet.max_column - 8 or i == sheet.max_column - 5:
+                chart2.append(value=series)
             else:
-                chart.append(value=series)
+                chart1.append(value=series)
 
-        chart.x_axis.majorGridlines = None
-        chart.y_axis.title = 'Voltage(mV)/Current(mA)/RemCap(mAh)/FullChgCap(mAh)'
+        # 关闭y轴框线
+        chart1.x_axis.majorGridlines = None
+        # 主y轴标题
+        chart1.y_axis.title = 'Voltage(mV)/Current(mA)/RemCap(mAh)/FullChgCap(mAh)'
 
-        chart_rsoc.y_axis.title = 'RSOC(%)'
-        chart_rsoc.y_axis.crosses = 'max'
-        chart_rsoc.y_axis.axId = 200
-        chart_rsoc.y_axis.majorGridlines = None
-        chart_rsoc.x_axis.majorGridlines = None
+        # 副y轴标题
+        chart2.y_axis.title = 'RSOC(%)/Temperature(\'C)'
+        # 表示在x轴最大值处建立新的y轴
+        chart2.y_axis.crosses = 'max'
+        chart2.y_axis.axId = 200
+        # 关闭y轴和x轴的框线
+        chart2.y_axis.majorGridlines = None
+        chart2.x_axis.majorGridlines = None
 
-        chart += chart_rsoc
-
-        chart_sheet.add_chart(chart)
+        chart1 += chart2
+        chart_sheet.add_chart(chart1)
 
         file.save(self.excel_path)
-
         file.close()
 
 
@@ -309,15 +346,16 @@ def main():
     global g_author
     global g_chr_voltage
     global g_fw_version
+    global g_project_name
 
     file_name = get_file_name()
-    excel_name = input('请输入导出Excel表格文件名（不需要添加后缀）：')
+    g_project_name = input('请输入项目名称：')
     g_author = input('请输入作者：')
     g_fw_version = input('请输入软件版本：')
-    g_chr_voltage = input('请输入充电电压：')
+    g_chr_voltage = input('请输入充电电压 (V)：')
     g_term_voltage = int(input('输入term_voltage (mV)：'))
 
-    build_excel = BuildExcel(file_name, excel_name)
+    build_excel = BuildExcel(file_name)
     time_count_thread = threading.Thread(target=time_count)
     time_count_thread.daemon = True
     time_count_thread.start()
